0460526d9a5556a978c3863aca91ffb5465b7288
Joel Beckum <joelbeckum@gmail.com>
2021-06-01 14:31:21 -0400

    completed Self-Driving Cars and Even or Odd assignments

--- practice.js
+++ practice.js
@@ -18,8 +18,37 @@
 // const myFavorite = logFavoriteBreed("golden retriever")
 // console.log(`When it comes to pets, ${myFavorite}`)
 
-const add = (first, second, third) => {
-    return first + second + third
+// const add = (first, second, third) => {
+//     return first + second + third
+// }
+
+// console.log(add(17, 4, 11))
+
+const go = (direction, speed) => {
+    let message = `The car is moving ${direction} at ${speed} mph.`
+
+    if (speed > 75) {
+        let speedMessage = message.concat(" SLOW DOWN!")
+        console.log(speedMessage)
+        return
+    }
+
+    console.log(message)
 }
 
-console.log(add(17, 4, 11))
+go("in circles", 12)
+go("forward", 85)
+
+const evenOrOdd = (number) => {
+    if ((number % 2) === 0) {
+        return "even"
+    }
+
+    return "odd"
+}
+
+const testArr = [3, 4, 17, 1, 260, 0]
+
+for (let num of testArr) {
+    console.log(evenOrOdd(num))
+}
